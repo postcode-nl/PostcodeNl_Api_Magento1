61df6aabadb17697f9bb9e4884bb4258788ae063
Jozef Verhoef <jozef@parse.nl>
2020-02-12 10:40:23 +0100

    Cleaned up Mage42_PostcodeNL_Model_System_Config_Source_AutocompleteCountry

--- app/code/community/Mage42/PostcodeNL/Model/System/Config/Source/AutocompleteCountry.php
+++ app/code/community/Mage42/PostcodeNL/Model/System/Config/Source/AutocompleteCountry.php
@@ -2,23 +2,26 @@
 
 class Mage42_PostcodeNL_Model_System_Config_Source_AutocompleteCountry extends Mage_Core_Model_Config_Data
 {
-    protected $_options;
-
-    public function toOptionArray($isMultiselect=false)
+    public function toOptionArray($isMultiselect = false)
     {
+        $response = Mage::helper('mage42_postcodenl')->_internationalGetSupportedCountries();
+
+        if (isset($response['mage42_postcodenl_message']))
+        {
+            return [
+                ['value' => 'error', 'label' => $response['mage42_postcodenl_message']]
+            ];
+        }
+
         $result = array();
-        if (!$this->_options) {
-            $jsonData = Mage::helper('mage42_postcodenl')->_internationalGetSupportedCountries();
-            if (isset($jsonData['mage42_postcodenl_message']))
-                return array(
-                    array('value' => 'error', 'label' => 'Please check your API credentials')
-                );
-            if (sizeof($jsonData) > 0)
-                $this->_options['countries'] = $jsonData;
+        foreach ($response as $country)
+        {
+            $result[] = array(
+                'value' => $country['iso2'] . '-' . $country['iso3'],
+                'label' => $country['name']
+            );
         }
-        for ($i = 0; $i < sizeof($this->_options['countries']); $i++) {
-            array_push($result, array('value' => $this->_options['countries'][$i]['iso2'] . '-' . $this->_options['countries'][$i]['iso3'], 'label' => $this->_options['countries'][$i]['name']));
-        }
+
         return $result;
     }
 }
